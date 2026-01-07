14938465c7f9faea0843ae14e8da571905923eb8
Nambona Adeline <adeline.p@pm.me>
2026-01-07 20:51:15 +0100

    Add simple HTML UI

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from backend.api.projects import router as projects_router
 from backend.api.uploads import router as uploads_router
 from backend.api.templates import router as templates_router
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="DMS")
 
@@ -16,3 +18,9 @@ def root():
 app.include_router(projects_router)                 # /projects
 app.include_router(uploads_router, prefix="/upload")# /upload/{project_id}
 app.include_router(templates_router, prefix="/templates")
+
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+@app.get("/")
+def home():
+    return FileResponse("frontend/index.html")
